78ef01db6b49a8f4e478a76787c891c80438f178
yusuf version <yusufef698@gmail.com>
2025-05-04 20:24:40 +0700

    Add project files.

---
+++ TugasPraktikum1/KonversiSuhu.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Nama     : Irma Sena Marliyana
+// NPM      : 230202084
+// Kelas    : TI 2D
+// Tugas    : Konversi Suhu
+
+namespace TugasPraktikum1
+{
+    class KonversiSuhu
+    {
+        static void Main()
+        {
+            Console.Write("Masukkan suhu dalam Celcius: ");
+            string input = Console.ReadLine();
+
+            short celcius = Convert.ToInt16(input);
+
+            double fahrenheit = (celcius * 9 / 5) + 32;
+            double reamur = celcius * 4 / 5;
+            double kelvin = celcius + 273.15;
+
+            Console.WriteLine($"{celcius}°C = {fahrenheit}°F");
+            Console.WriteLine($"{celcius}°C = {reamur}°R");
+            Console.WriteLine($"{celcius}°C = {kelvin} K");
+        }
+    }
+}
---
+++ TugasPraktikum1/KonversiWaktu.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Nama     : Irma Sena Marliyana
+// NPM      : 230202084
+// Kelas    : TI 2D
+// Tugas    : Konversi Waktu
+
+namespace TugasPraktikum1
+{
+    class KonversiWaktu
+    {
+        static void Main()
+        {
+            int totalDetik, jam, menit, detik;
+
+            Console.Write("Masukkan jumlah detik: ");
+            totalDetik = Convert.ToInt16(Console.ReadLine());
+
+            jam = totalDetik / 3600;
+            int sisa = totalDetik % 3600;
+            menit = sisa / 60;
+            detik = sisa % 60;
+
+            Console.WriteLine("Hasil konversi: {0} jam, {1} menit, {2} detik", jam, menit, detik);
+        }
+    }
+}
